ec2f78bcbef5b581b19481dc5dcd24215ea30a6c
Lauren Dove <elledoveinc@gmail.com>
2019-09-21 03:15:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 def my_collect (people)
 newest = []
- help = people do |m| m.split(" ").first                             #people.collect do |m| m.upcase
+ help = people do |m| m.split(" ").first 
+ final = help
+ #people.collect do |m| m.upcase
 newest <<  help
 end
 end
