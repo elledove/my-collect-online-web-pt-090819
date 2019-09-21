7483b10f6b7f030ab06bcad6ca9b1b1bf6207648
Lauren Dove <elledoveinc@gmail.com>
2019-09-21 03:50:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,18 @@
 require 'pry'
 
-def my_collect (people)
- 
- people.collect do |m| m.split ( " " )
+def my_collect(people)
+ i = 0    
+ newest = []
+ while i < people.length do 
+   
+   
+   
  end
+ 
+ 
+ 
+ #people.collect do |m| m.split ( " " )
+ #end
 end
 
 
